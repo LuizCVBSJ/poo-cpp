bfc0f358abcad4c7eddcaf9833f3fc76f74c3872
Luiz CVBSJ <luizcvbsj@gmail.com>
2024-03-12 14:39:26 +0100

    added esercizio3.cpp but did not finish the exercise

---
+++ Programmazione ad Oggetti C++/Luiz/Lab2 05-03-2024/esercizio3.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <string>
+namespace std;
+
+class Money
+/*  Un operatore ‘+’ definito come funzione non-membro: Money operator+(Money m1, Money m2)
+che esegue la somma di due oggetti Money sommando euro e centesimi (i centesimi se
+eccedono il valore di 100 vanno convertiti in euro: es 10.50 euro+ 5.70 euro= 16.20 euro)
+ Un operatore ‘<<’ definito come funzione non-membro: ostream& operator<<(ostream& os,
+Money m) per stampare a video i dati membri euros e cents
+*/
+{
+  private:
+  {
+    int euros;
+    int cents;
+  }
+  public:
+    Money(int new_euros = 0, int new_cents = 0)
+    {
+      euros = new_euros;
+      cents = new_cents;
+    }
+    int get_euros()
+    {
+      return euros;
+    }
+    void set_euros(int new_euros)
+    {
+      euros = new_euros
+    }
+    int get_cents()
+    {
+      return cents;
+    }
+    void set_cents(int new_cents)
+    {
+      cents = new_cents;
+    }
+};
+
+
+class CreditCard
+{
+  private:
+  {
+    string owner;
+    string card_number;
+    Money Total_paid;
+  }
+  public:
+  {
+    CreditCard(string new_owner, string new_card_number)
+    {
+      owner = new_owner;
+      card_number = new_card_number;  
+    }
+    void print()
+    {
+      cout << "Owner: " << owner << "Card Number: " << card_number << "\n";
+    }
+    Money get_total_charges()
+    {
+      return Total_paid;
+    }
+    void charge(int euro, int cents)
+    {
+      int new_euro;
+      int new_cents = cents + Total_paid.get_cents();
+      if new_cents >= 100:
+      {
+        new_euro = 1;
+        new_cents -= 100;
+      }
+      Total_paid.set_cents(new_cents) 
+
+
+    }
+  }
+};
