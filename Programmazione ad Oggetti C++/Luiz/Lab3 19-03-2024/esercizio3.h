ce8ee0dcdc66143cf75b9355ecaed49a9e68bd56
Luiz CVBSJ <luizcvbsj@gmail.com>
2024-03-19 14:24:01 +0100

    added esercizio3.cpp and it's header file but did not finish the exercise

---
+++ Programmazione ad Oggetti C++/Luiz/Lab3 19-03-2024/esercizio3.h
@@ -0,0 +1,21 @@
+/*
+Scrivere una classe template template<class T> class Pair i cui oggetti rappresentino coppie di
+elementi (“first” e “second”) dello stesso tipo generico T. La classe oltre ai costruttori deve contenere i
+seguenti metodi pubblici:
+ void set_element(int position, T value); // imposta value in posizione 1 o 2
+ T get_element(int position) const; // restituisce l’elemento in posizione 1 o 2
+ void add_up(const Pair<T>& the_pair); // somma gli elementi corrispondenti di un
+oggetto Pair con quelli contenuti nell’argomento the_pair 
+*/
+#ifndef ES3 
+#define ES3
+
+#include <iostream>
+using namespace std
+
+template <class T> class Pair
+{
+
+}
+
+#endif
