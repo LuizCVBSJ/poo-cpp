e12977cd41c702598ee2ac91205492e7435bb24d
luiz <luizcvbsj@gmail.com>
2024-04-30 10:47:35 +0200

    modified esempio_1_21.cpp because 'using namespace std;' was included but the previous version of the file still contained 'std::' declarations, removed for better readability

--- Programmazione ad Oggetti C++/Esercizi svolti a lezione/introduzioneC++/esempio_1_21.cpp
+++ Programmazione ad Oggetti C++/Esercizi svolti a lezione/introduzioneC++/esempio_1_21.cpp
@@ -6,8 +6,8 @@ using namespace std;
 
 int main ()
 {
-  string pi = "pi is " + std::to_string(3.1415926);
-  string perfect = std::to_string(1+2+4+7+14) + " is a number";
+  string pi = "pi is " + to_string(3.1415926);
+  string perfect = to_string(1+2+4+7+14) + " is a number";
   cout << pi << '\n';
   cout << perfect << '\n';
 
